9e8916582476f885748d42df548cfebbc62ca7c9
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2018-02-26 12:08:07 +0300

    26/02 12.00

--- js/contacts.js
+++ js/contacts.js
@@ -3,17 +3,246 @@
 $(document).ready(function(){
   
   new WOW().init();
+/*HOVER OVER IMG*/
+  $('.img_block.japan').hover(
+    function(){
+      $('.img_block.japan').animate({opacity:"1", saturate:"100%"});
+      $("#target1").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.japan').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.japan').animate({opacity:".5", saturate:"0%"});
+      $("#target1").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.japan').animate({opacity:".5", saturate:"0%"});
+    }
+  );
 
-  $(window).scroll(function() {
-    $('#animatedElement').each(function(){
-    var imagePos = $(this).offset().top;
+  $('.img_block.norway').hover(
+    function(){
+      $('.img_block.norway').animate({opacity:"1", saturate:"100%"});
+      $("#target2").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.norway').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.norway').animate({opacity:".5", saturate:"0%"});
+      $("#target2").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.norway').animate({opacity:".5", saturate:"0%"});
+    }
+  );
 
-    var topOfWindow = $(window).scrollTop();
-      if (imagePos < topOfWindow+400) {
-        $(this).addClass("slideUp");
-      }
-    });
-  });
+  $('.img_block.china').hover(
+    function(){
+      $('.img_block.china').animate({opacity:"1", saturate:"100%"});
+      $("#target3").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.china').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.china').animate({opacity:".5", saturate:"0%"});
+      $("#target3").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.china').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+  $('.img_block.usa').hover(
+    function(){
+      $('.img_block.usa').animate({opacity:"1", saturate:"100%"});
+      $("#target4").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.usa').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.usa').animate({opacity:".5", saturate:"0%"});
+      $("#target4").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.usa').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+  $('.img_block.southAfrica').hover(
+    function(){
+      $('.img_block.southAfrica').animate({opacity:"1", saturate:"100%"});
+      $("#target5").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.southAfrica').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.southAfrica').animate({opacity:".5", saturate:"0%"});
+      $("#target5").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.southAfrica').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+  $('.img_block.brasil').hover(
+    function(){
+      $('.img_block.brasil').animate({opacity:"1", saturate:"100%"});
+      $("#target6").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.brasil').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.brasil').animate({opacity:".5", saturate:"0%"});
+      $("#target6").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.brasil').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+/*HOVER OVER TRGET ICON*/
+
+  $('#target1').hover(
+    function(){
+      $('.img_block.japan').animate({opacity:"1", saturate:"100%"});
+      $("#target1").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.japan').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.japan').animate({opacity:".5", saturate:"0%"});
+      $("#target1").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.japan').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+  $('#target2').hover(
+    function(){
+      $('.img_block.norway').animate({opacity:"1", saturate:"100%"});
+      $("#target2").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.norway').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.norway').animate({opacity:".5", saturate:"0%"});
+      $("#target2").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.norway').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+  $('#target3').hover(
+    function(){
+      $('.img_block.china').animate({opacity:"1", saturate:"100%"});
+      $("#target3").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.china').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.china').animate({opacity:".5", saturate:"0%"});
+      $("#target3").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.china').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+  $('#target4').hover(
+    function(){
+      $('.img_block.usa').animate({opacity:"1", saturate:"100%"});
+      $("#target4").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.usa').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.usa').animate({opacity:".5", saturate:"0%"});
+      $("#target4").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.usa').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+  $('#target5').hover(
+    function(){
+      $('.img_block.southAfrica').animate({opacity:"1", saturate:"100%"});
+      $("#target5").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.southAfrica').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.southAfrica').animate({opacity:".5", saturate:"0%"});
+      $("#target5").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.southAfrica').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+  $('#target6').hover(
+    function(){
+      $('.img_block.brasil').animate({opacity:"1", saturate:"100%"});
+      $("#target6").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.brasil').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.brasil').animate({opacity:".5", saturate:"0%"});
+      $("#target6").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.brasil').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+/*HOVER OVER DESCRIPTION*/
+
+$('.description_block.japan').hover(
+    function(){
+      $('.img_block.japan').animate({opacity:"1", saturate:"100%"});
+      $("#target1").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.japan').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.japan').animate({opacity:".5", saturate:"0%"});
+      $("#target1").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.japan').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+  $('.description_block.norway').hover(
+    function(){
+      $('.img_block.norway').animate({opacity:"1", saturate:"100%"});
+      $("#target2").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.norway').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.norway').animate({opacity:".5", saturate:"0%"});
+      $("#target2").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.norway').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+  $('.description_block.china').hover(
+    function(){
+      $('.img_block.china').animate({opacity:"1", saturate:"100%"});
+      $("#target3").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.china').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.china').animate({opacity:".5", saturate:"0%"});
+      $("#target3").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.china').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+  $('.description_block.usa').hover(
+    function(){
+      $('.img_block.usa').animate({opacity:"1", saturate:"100%"});
+      $("#target4").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.usa').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.usa').animate({opacity:".5", saturate:"0%"});
+      $("#target4").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.usa').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+  $('.description_block.southAfrica').hover(
+    function(){
+      $('.img_block.southAfrica').animate({opacity:"1", saturate:"100%"});
+      $("#target5").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.southAfrica').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.southAfrica').animate({opacity:".5", saturate:"0%"});
+      $("#target5").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.southAfrica').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+  $('.description_block.brasil').hover(
+    function(){
+      $('.img_block.brasil').animate({opacity:"1", saturate:"100%"});
+      $("#target6").css('backgroundImage', 'url(img/target-active.png)');
+      $('.description_block.brasil').animate({opacity:"1", saturate:"100%"});
+    },
+    function(){
+      $('.img_block.brasil').animate({opacity:".5", saturate:"0%"});
+      $("#target6").css('backgroundImage', 'url(img/target-inactive.png)');
+      $('.description_block.brasil').animate({opacity:".5", saturate:"0%"});
+    }
+  );
+
+
 
 });
 
--- js/parts.js
+++ js/parts.js
@@ -7,6 +7,159 @@ $(document).ready(function(){
 
 
 
+  var boxVis = $('.preview_block').is(':visible');
+  $('.catalog_card.turbine').click(function(){
+  	
+  	if ('.preview_block:visible') {
+  		$('.preview_block').hide();
+  		$('.preview_block.turbine').show();
+  	}
+  	if ('.preview_block:hidden') {
+  		$('.preview_block.turbine').slideToggle();
+  	}
+
+  	
+  	else{}
+  	/*var elemVis =  $('.preview_block.turbine').is(':visible');
+  	if (elemVis = 1) {
+  		$('.preview_block').css('display', 'none');
+  	}
+  	if (boxVis = 0){
+  		$('.preview_block.turbine').slideDown();
+  	}
+  	
+  	else {
+  		$('.preview_block').css('display', 'none');
+  		$('.preview_block.turbine').css('display', 'block');}*/
+  });
+
+  $('.catalog_card.starter').click(function(){
+  	if (boxVis = 0){
+  		$('.preview_block.starter').slideDown();
+  	}
+  	else {
+  		$('.preview_block').css('display', 'none');
+  		$('.preview_block.starter').css('display', 'block');}
+  });
+
+  $('.catalog_card.generator').click(function(){
+  	if (boxVis = 0){
+  		$('.preview_block.generator').slideDown();
+  	}
+  	else {
+  		$('.preview_block').css('display', 'none');
+  		$('.preview_block.generator').css('display', 'block');}
+  });
+
+  $('.catalog_card.piston').click(function(){
+  	if (boxVis = 0){
+  		$('.preview_block.piston').slideDown();
+  	}
+  	else {
+  		$('.preview_block').css('display', 'none');
+  		$('.preview_block.piston').css('display', 'block');}
+  });
+
+  $('.catalog_card.cylinder').click(function(){
+  	if (boxVis = 0){
+  		$('.preview_block.cylinder').slideDown();
+  	}
+  	else {
+  		$('.preview_block').css('display', 'none');
+  		$('.preview_block.cylinder').css('display', 'block');}
+  });
+
+  $('.catalog_card.valve').click(function(){
+  	if (boxVis = 0){
+  		$('.preview_block.valve').slideDown();
+  	}
+  	else {
+  		$('.preview_block').css('display', 'none');
+  		$('.preview_block.valve').css('display', 'block');}
+  });
+
+  $('.catalog_card.crankshaft').click(function(){
+  	if (boxVis = 0){
+  		$('.preview_block.crankshaft').slideDown();
+  	}
+  	else {
+  		$('.preview_block').css('display', 'none');
+  		$('.preview_block.crankshaft').css('display', 'block');}
+  });
+
+  $('.catalog_card.cam-shaft').click(function(){
+  	if (boxVis = 0){
+  		$('.preview_block.cam-shaft').slideDown();
+  	}
+  	else {
+  		$('.preview_block').css('display', 'none');
+  		$('.preview_block.cam-shaft').css('display', 'block');}
+  });
+
+  $('.catalog_card.piston-rod').click(function(){
+  	if (boxVis = 0){
+  		$('.preview_block.piston-rod').slideDown();
+  	}
+  	else {
+  		$('.preview_block').css('display', 'none');
+  		$('.preview_block.piston-rod').css('display', 'block');}
+  });
+
+  $('.catalog_card.water-pump').click(function(){
+  	if (boxVis = 0){
+  		$('.preview_block.water-pump').slideDown();
+  	}
+  	else {
+  		$('.preview_block').css('display', 'none');
+  		$('.preview_block.water-pump').css('display', 'block');}
+  });
+
+  $('.catalog_card.oil-pump').click(function(){
+  	if (boxVis = 0){
+  		$('.preview_block.oil-pump').slideDown();
+  	}
+  	else {
+  		$('.preview_block').css('display', 'none');
+  		$('.preview_block.oil-pump').css('display', 'block');}
+  });
+
+  $('.catalog_card.oil-radiator').click(function(){
+  	if (boxVis = 0){
+  		$('.preview_block.oil-radiator').slideDown();
+  	}
+  	else {
+  		$('.preview_block').css('display', 'none');
+  		$('.preview_block.oil-radiator').css('display', 'block');}
+  });
+
+  $('.catalog_card.injection').click(function(){
+  	if (boxVis = 0){
+  		$('.preview_block.injection').slideDown();
+  	}
+  	else {
+  		$('.preview_block').css('display', 'none');
+  		$('.preview_block.injection').css('display', 'block');}
+  });
+
+  $('.catalog_card.cylinder-head').click(function(){
+  	if (boxVis = 0){
+  		$('.preview_block.cylinder-head').slideDown();
+  	}
+  	else {
+  		$('.preview_block').css('display', 'none');
+  		$('.preview_block.cylinder-head').css('display', 'block');}
+  });
+
+  $('.catalog_card.MPC').click(function(){
+  	if (boxVis = 0){
+  		$('.preview_block.MPC').slideDown();
+  	}
+  	else {
+  		$('.preview_block').css('display', 'none');
+  		$('.preview_block.MPC').css('display', 'block');}
+  });
+
+
 });
 
 
