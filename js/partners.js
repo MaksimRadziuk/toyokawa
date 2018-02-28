068845979e0c0f2f5865f0c65e6e7c5813a95ecd
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2018-02-28 14:15:52 +0300

    28/02 14.15

--- js/partners.js
+++ js/partners.js
@@ -16,13 +16,35 @@ $(document).ready(function(){
   });
 
   $(".partners_block").each(function(){ 
-      var h1 = $(this).height();
-      var h = $(this).parent(".partners_row").height(); 
-      if (h > h1){
-        $(this).css('height', h);
-      }
-      else{}
-    });  
+    var h1 = $(this).height();
+    var h = $(this).parent(".partners_row").height(); 
+    if (h > h1){
+      $(this).css('height', h);
+    }
+    else{}
+  });  
+
+
+  $('.partners_block>h4').hover(function(){
+    $(this).parent('.partners_block').addClass('hover');
+  },
+  function(){
+    $(this).parent('.partners_block').removeClass('hover')
+  });
+
+  $('.partners_block>.logo').hover(function(){
+    $(this).parent('.partners_block').addClass('hover');
+  },
+  function(){
+    $(this).parent('.partners_block').removeClass('hover')
+  });
+
+  $('.partners_block>.description').hover(function(){
+    $(this).parent('.partners_block').addClass('hover');
+  },
+  function(){
+    $(this).parent('.partners_block').removeClass('hover')
+  });
 
 });
 
